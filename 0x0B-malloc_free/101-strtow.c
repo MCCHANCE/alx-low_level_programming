dab82769140ffdda34aa768fd21c022faf95ff0f
MCCHANCE <unwananyong2021@gmail.com>
2023-03-14 12:15:25 -0700

    101-strtow.c

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -14,12 +14,12 @@ int count_word(char *s)
 			flag = 0;
 				w = 0;
 
-					for c = 0; s[c] != '\0'; c++;
+					for (c = 0; s[c] != '\0'; c++);
 							{
-if s[c] == ' ';
+if (s[c] == ' ');
 
 			 flag = 0;
-else if flag == 0;
+if (flag == 0);
 												{
 																flag = 1;
 																			w++;
