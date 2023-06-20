5e75c2b5a050c0431f46cae8d165d83c66d9b07e
MCCHANCE <unwananyong2021@gmail.com>
2023-06-20 03:49:51 -0700

    9-strcpy.c

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -6,18 +6,18 @@
  * @src: copy from
  * Return: string
  */
-char *_strcpy(char *dest, char src)
+char *_strcpy(char *dest, char *src)
 {
-	int (1 = 0);
+	int i = 0;
 	int x = 0;
 
-	while ((src + 1) != '\0')
+	while (*(src + 1) != '\0')
 	{
-		1++;
+		i++;
 	}
-	for ( ; x < 1 ; x++)
+	for ( ; x < i ; x++)
 	{
-		dest[x] = src(x);
+		dest[x] = src[x];
 	}
 	dest[1] = '\0';
 	return (dest);
