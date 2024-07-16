028a846a3872e3bf1c5793c8994918bef4186cb6
MCCHANCE <unwananyong2021@gmail.com>
2024-07-16 13:27:42 -0700

    0-linear.c

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -1,4 +1,6 @@
+#ifndef SEARCH_ALGOS_H
 #define SEARCH_ALGOS_H
+
 #include <stddef.h>
 #include <stdio.h>
 
@@ -10,22 +12,25 @@
  *
  * Return: The first index of the value in the array, otherwise -1.
  */
-
 int linear_search(int *array, size_t size, int value)
 {
-	size_t i;
+    size_t i;
 
-	for (i = 0; (i < size) && (array); i++)
-	{
-		if (*(array + i) == value)
-		{
-			printf("Value checked array[%d] = [%d]\n", (int)i, *(array + i));
-			return (i);
-		}
-		else
-		{
-			printf("Value checked array[%d] = [%d]\n", (int)i, *(array + i));
-		}
-	}
-	return (-1);
+    if (array == NULL)
+    {
+        return (-1);
+    }
+
+    for (i = 0; i < size; i++)
+    {
+        printf("Value checked array[%zu] = [%d]\n", i, array[i]);
+        if (array[i] == value)
+        {
+            return (i);
+        }
+    }
+    return (-1);
 }
+
+#endif /* SEARCH_ALGOS_H */
+
